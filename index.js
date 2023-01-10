9377a3827f21aae02e7f75928e1b37f786301202
teeg0X <122372410+teeg0X@users.noreply.github.com>
2023-01-10 17:04:04 +0300

    Menu

--- index.js
+++ index.js
@@ -1,96 +1,19 @@
-// // let a = +prompt("Введите число a");
-// // let b = +prompt("Введите число b");
-// // let summ = prompt("Какую операцию произвести? 1)Сложение, 2)Вычитание , 3)Умножение , 4)Деление ");
+// console.log($);
 
-let calc = {
-    
-}
-
-
-$('#btn-m').click(function(e){
+$('.btn').click(function(e){
     e.preventDefault()
-    let numberA = Number($('#inputA').val());
-    let numberB = Number($('#inputB').val());
-    let summ = $('#method').val();
-    let history = $('textarea').val();
-    switch(summ){
-        case "S":
-        $('textarea').val(history + '\n' + ("Ваше сложение: " + numberA + " + " + numberB + " = " + (numberA + numberB)))
-        break;
-        
-        case "V":
-        $('textarea').val(history + '\n' + ("Ваше сложение: " + numberA + " - " + numberB + " = " + (numberA - numberB)));
-        break;
-    
-        case "Y":
-        $('textarea').val(history + '\n' + ("Ваше сложение: " + numberA + " * " + numberB + " = " + (numberA * numberB)));
-        break;
-    
-        case "D":
-        $('textarea').val(history + '\n' + ("Ваше сложение: " + numberA + " : " + numberB + " = " + (numberA / numberB)));
-        break;
-        
-    }
-})
+    $('.container-save').slideDown(1000);
 
-
-let top_p = $('#position-t');
-let left_p = $('#position-l');
-position(top_p, left_p);
-
-function position(top_p, left_p){
-    left_p.on('change', function(e){
-        e.preventDefault();
-        let base = `${left_p.val()}px`
-        $('form').css({
-            'padding-left' : base,
-        })
-    })
-    top_p.on('change', function(e){
-        e.preventDefault(e);
-        let base2 = `${top_p.val()}px`
-        $('form').css({
-            'padding-top' : base2,
-        })
-    })
-}
-
-$('#color').on('change', function(e){
-    e.preventDefault();
-    let main_color = $('#color').val()
-    let swap = $('body')
-    let black = '#000000'
-    let white = '#fff'
-    
-    switch(main_color){
-        case 'black':
-            swap.css({
-                'background-color' : black,
+    $('input').each(function(){
+        if($(this).val()){
+            $(this).css({
+                'border' : '1px solid grey'
             })
-            break;
-        case 'white':
-            swap.css({
-                'background-color' : white,
+            
+        }else{
+            $(this).css({
+                'border' : '1px solid red'
             })
-            break;
-    }
-
+        }
+    })
 })
-
-
-$("#btn-history").click(function(e){
-    e.preventDefault()
-    $("textarea").val("");
-})
-
-$('#btn-reset').click(function(e){
-    e.preventDefault()
-    $('#inputA').val("");
-    $('#inputB').val("");
-})
-
-
-
-
-
-
